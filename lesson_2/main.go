b5ad349bf632f11c4e60fc01e45cbe3220db7486
Evgeniy <n0fr1@mail.ru>
2021-01-26 20:23:38 +0300

    add decimal to binary

--- lesson_2/main.go
+++ lesson_2/main.go
@@ -6,14 +6,44 @@ import (
 	"fmt"
 	"os"
 	"strconv"
+	"strings"
 )
 
 func main() {
 
-	findExpFor() //2. степень через цикл
-	findExpRec() //2. степень через рекурсию
+	getStringToNum() //1. Перевод из 10-ричной в 2-двоичную систему (через цикл)
+	findExpFor()     //2. степень через цикл
+	findExpRec()     //2. степень через рекурсию. Плюс использование свойства четности степени.
 }
 
+//1. Перевод из 10-ричной в 2-двоичную систему (через цикл)
+func getStringToNum() {
+
+	var slice []int
+	var result int
+	var stringNum string
+
+	str := "16,8,4,2,1"
+	strs := strings.Split(str, ",") // "," - это разделитель строки
+
+	for _, s := range strs {
+		num, err := strconv.Atoi(s)
+		if err != nil {
+			fmt.Fprint(os.Stderr, "Вы ввели не число! \n")
+			os.Exit(1)
+		}
+		slice = append(slice, num) //добавляем в слайс
+	}
+
+	for i := len(slice) - 1; i >= 0; i-- { //обходим с конца и преобразуем в строку
+
+		result = slice[i] % 2
+		stringNum = stringNum + strconv.Itoa(result)
+	}
+	fmt.Println(stringNum)
+}
+
+//2. степень через цикл
 func findExpFor() {
 
 	var result float64
@@ -40,11 +70,11 @@ func findExpFor() {
 	result = 1
 	stInt := int(st)
 
-	if st > 0 {
+	if st > 0 { //положительная степень
 		for i := 0; i < stInt; i++ {
 			result = result * num
 		}
-	} else {
+	} else { //отрицательная степень
 		for i := -1; i > stInt; i-- {
 			result = 1 / (result * (-num))
 		}
@@ -54,6 +84,7 @@ func findExpFor() {
 
 }
 
+//2. степень через рекурсию
 func findExpRec() {
 
 	var result float64
@@ -72,26 +103,41 @@ func findExpRec() {
 		os.Exit(1)
 	}
 
-	if st < 0 {
+	intSt := int(st)
+	parity := intSt % 2 //проверяем на четность степени. Если степень четная - то можно использовать четность степени.
+	if parity == 0 {
+		st = st / 2
+	}
+
+	if st < 0 { //указана отрицательная степень
+
 		result = recMinus(num, st)
+		if parity == 0 {
+			result = result * result
+		}
+
 		fmt.Println(result)
 		return
 	}
 
-	result = rec(num, st)
+	result = rec(num, st) //указана положительная степень
+	if parity == 0 {
+		result = result * result
+	}
 	fmt.Println(result)
-
 }
 
+//отрицательная степень
 func recMinus(num, st float64) float64 {
 
 	if st == 0 {
 		return num
 	}
 
-	return (1 / num) * (recMinus(num, st+1)) // -2
+	return (1 / num) * (recMinus(num, st+1))
 }
 
+//положительная степень
 func rec(num, st float64) float64 {
 
 	if st == 0 {
