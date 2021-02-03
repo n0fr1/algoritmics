f785ffe99568910f6694d445e8d2a63374d518f1
Evgeniy <n0fr1@mail.ru>
2021-02-03 17:31:14 +0300

    add test for chessmate

--- lesson_4/main.go
+++ lesson_4/main.go
@@ -2,276 +2,137 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"os"
-	"reflect"
-	"strconv"
 )
 
-//Variants is ...
-type Variants struct {
-	A, B, C, D, E int64
+//Userchoise is ...
+type Userchoise struct {
+	Cname  string
+	Cnum   int64
+	Cindex int
 }
 
-//Choise is ...
-type Choise struct {
-	Name  string
-	Index int
+//Choises is ...
+type Choises []Userchoise
+
+//Vary is ...
+type Vary struct {
+	Name string
+	Num  []int64
 }
+
+//Coordinats is ...
+type Coordinats []Vary
 
 func main() {
 
-	var a, b int64 // //3. Обход конём шахматной доски 5х5. Наброски
+	var x1, x2 int64 //исходим из того, что будут заданы начальные координаты для двух из четырех клеток первого стартового хода коня.
 
 	// fmt.Println("Укажите стартовые клетки для положения коня")
 	// fmt.Println("Введите первый номер: ")
-	// a, err := getNumInt() //проверяем на число
+	// x1, err := getNumInt() //проверяем на число
 	// if err != nil {
 	// 	fmt.Fprint(os.Stderr, "Вы ввели не число! \n")
 	// 	os.Exit(1)
 	// }
 
 	// fmt.Println("Введите второй номер: ")
-	// b, err := getNumInt() //проверяем на число
+	// x2, err := getNumInt() //проверяем на число
 	// if err != nil {
 	// 	fmt.Fprint(os.Stderr, "Вы ввели не число! \n")
 	// 	os.Exit(1)
 	// }
 
-	a = 1 //для теста
-	b = 2
+	p := new() //случай для доски 5х5. Пока указываем явно, потом переделаем.
 
-	var v = []Variants{}
-	v = []Variants{
-		{1, 2, 3, 4, 5},
-		{6, 7, 8, 9, 10},
-		{11, 12, 13, 14, 15},
-		{16, 17, 18, 19, 20},
-		{21, 22, 23, 24, 25},
+	num1 := []int64{1, 2, 3, 4, 5}
+	num2 := []int64{6, 7, 8, 9, 10}
+	num3 := []int64{11, 12, 13, 14, 15}
+	num4 := []int64{16, 17, 18, 19, 20}
+	num5 := []int64{21, 22, 23, 24, 25}
+
+	p.add("A", num1)
+	p.add("B", num2)
+	p.add("C", num3)
+	p.add("D", num4)
+	p.add("E", num5)
+
+	x1 = 3 //для теста
+	x2 = 2
+
+	testPosition, c := addChoice(p, x1, x2) //вся эта проверка - на соседство выбранных клеток. Также определяем местоположение на шахматной доске.
+	if !(testPosition) {
+		fmt.Println("Введены не верные начальные координаты!") //проверка на соседство клеточек не пройдена
+		return
 	}
-
-	testFunc(&v, a, b)
-
-	// v.slA = []int{1, 2, 3, 4, 5}
-	// v.slB = []int{6, 7, 8, 9, 10}
-	// v.slC = []int{11, 12, 13, 14, 15}
-	// v.slD = []int{16, 17, 18, 19, 20}
-	// v.slE = []int{21, 22, 23, 24, 25}
-
-	//test(&v, a, b)
-	//test2(v, a, b)
-
-	// var slice []int //занятые позиции
-	// slice = append(slice, a)
-	// slice = append(slice, b)
-
-	// //нумеруем строки - для случая 5x5
-	// slA := []int{1, 2, 3, 4, 5}
-	// slB := []int{6, 7, 8, 9, 10}
-	// slC := []int{11, 12, 13, 14, 15}
-	// slD := []int{16, 17, 18, 19, 20}
-	// slE := []int{21, 22, 23, 24, 25}
-
-	//проверить на правильный ввод стартовой пары
-	// if (a-b) == 1 || (b-a) == 1 { //принадлежат одной строке
-	// 	fmt.Println("ок")
-	// 	return
-	// }
-
-	// result := testInput(slA, slB, slC, slD, slE, a, b)
-	// if !result {
-	// 	fmt.Println("Не правильно указана стартовая пара")
-	// 	return
-	// }
-
-	//проверяем ходы со стороны а
-	//if
-
+	fmt.Println(c)
 }
 
-func testFunc(v *[]Variants, a int64, b int64) bool {
+func (p *Coordinats) add(name string, num []int64) {
 
-	for i := 0; i < len(*v); i++ {
-		val := reflect.ValueOf((*v)[i])
+	pnew := Vary{
+		Name: name,
+		Num:  num,
+	}
+	*p = append(*p, pnew)
+}
 
-		for k := 0; k < val.NumField(); k++ {
+//добавляем к выбору: название, номер строки и номер по порядку-индекс.
+func (c *Choises) addChoice(cname string, cnum int64, cindex int) {
+	cnew := Userchoise{
+		Cname:  cname,
+		Cnum:   cnum,
+		Cindex: cindex,
+	}
+	*c = append(*c, cnew)
+}
 
-			value := val.Field(k).Interface().(int64)
+func new() *Coordinats {
+	var arr Coordinats
+	return &arr
+}
 
-			if value == a {
+func newChoice() *Choises {
+	var arr Choises
+	return &arr
+}
 
-				typeField := val.Type().Field(k)
-				p1 := Choise{Name: typeField.Name, Index: k}
-				fmt.Println(p1)
+//сравниваем выбор пользователя с тем, что есть в таблице с номерами.
+func addChoice(p *Coordinats, x1 int64, x2 int64) (bool, *Choises) {
 
+	c := newChoice()
+
+	for _, pp := range *p {
+		for ind, zn := range pp.Num {
+			if zn == x1 || zn == x2 {
+				c.addChoice(pp.Name, zn, ind)
 			}
-
-			if value == b {
-				typeField := val.Type().Field(k)
-				p2 := Choise{Name: typeField.Name, Index: k}
-				fmt.Println(p2)
-			}
-
+		}
 	}
 
-}
+	x := (*c)[0] //Поскольку координаты - две. Поэтому обращаемся напрямую по индексу
+	y := (*c)[1]
 
-func getNumInt() (int, error) {
+	if x.Cname == y.Cname { //проверка на соседство двух клеточек шахматной доски. Если клетки находятся поодаль - (А1, B10) - то это ошибка и запускать программу нет смысла.
+		if (x.Cnum-y.Cnum) == 1 || (y.Cnum-x.Cnum) == 1 {
+			return true, c
+		}
 
-	input := bufio.NewScanner(os.Stdin)
-	input.Scan()
-
-	num, err := strconv.Atoi(input.Text())
-	return num, err
-
-}
-
-func test2(v Variants, a, b int) {
-
-	val := reflect.ValueOf(v)
-	for i := 0; i < val.NumField(); i++ {
-
-		//for _, zn := range val.Field(i) {
-
-		//}
-		// result := val.Field(i).Interface().(Variants)
-		// fmt.Println(result)
+	} else {
+		if x.Cindex == y.Cindex {
+			return true, c
+		}
 	}
+	return false, c
 }
 
-// func test(v *Variants, a, b int) bool {
+//проверяем, что ввели именно число
+// func getNumInt() (int, error) {
 
-// 	var indA, indB int
+// 	input := bufio.NewScanner(os.Stdin)
+// 	input.Scan()
 
-// 	for ind, zn := range v.slA {
-
-// 		if zn == a {
-// 			indA = ind
-// 		}
-// 		if zn == b {
-// 			indB = ind
-// 		}
-// 	}
-
-// 	for ind, zn := range v.slB {
-
-// 		if zn == a {
-// 			indA = ind
-// 		}
-// 		if zn == b {
-// 			indB = ind
-// 		}
-// 	}
-
-// 	for ind, zn := range v.slC {
-
-// 		if zn == a {
-// 			indA = ind
-// 		}
-// 		if zn == b {
-// 			indB = ind
-// 		}
-// 	}
-
-// 	for ind, zn := range v.slD {
-
-// 		if zn == a {
-// 			indA = ind
-// 		}
-// 		if zn == b {
-// 			indB = ind
-// 		}
-// 	}
-
-// 	for ind, zn := range v.slE {
-
-// 		if zn == a {
-// 			indA = ind
-// 		}
-// 		if zn == b {
-// 			indB = ind
-// 		}
-// 	}
-
-// 	if indA == indB {
-// 		return true //, indA, indB
-// 	}
-
-// 	return false
-// }
-
-// func testInput(slA, slB, slC, slD, slE []int, a, b int) bool {
-
-// 	var indA, indB int //, counter int //индексы со сдвигом двух чисел.
-// 	varMap := make(map[string]int)
-
-// 	//сюда добавляем мапу.
-
-// 	for ind, zn := range slA {
-
-// 		if zn == a {
-// 			indA = ind
-// 			varMap["slA"] = ind
-// 		}
-// 		if zn == b {
-// 			indB = ind
-// 			varMap["slA"] = ind
-// 		}
-// 	}
-
-// 	for ind, zn := range slB {
-
-// 		if zn == a {
-// 			indA = ind
-// 			varMap["slB"] = ind
-// 		}
-// 		if zn == b {
-// 			indB = ind
-// 			varMap["slB"] = ind
-// 		}
-// 	}
-
-// 	for ind, zn := range slC {
-
-// 		if zn == a {
-// 			indA = ind
-// 			varMap["slC"] = ind
-// 		}
-// 		if zn == b {
-// 			indB = ind
-// 			varMap["slC"] = ind
-// 		}
-// 	}
-
-// 	for ind, zn := range slD {
-
-// 		if zn == a {
-// 			indA = ind
-// 			varMap["slD"] = ind
-// 		}
-// 		if zn == b {
-// 			indB = ind
-// 			varMap["slD"] = ind
-// 		}
-// 	}
-
-// 	for ind, zn := range slE {
-
-// 		if zn == a {
-// 			indA = ind
-// 			varMap["slE"] = ind
-// 		}
-// 		if zn == b {
-// 			indB = ind
-// 			varMap["slE"] = ind
-// 		}
-// 	}
-
-// 	if indA == indB {
-// 		return true //, indA, indB
-// 	}
-
-// 	return false
+// 	num, err := strconv.Atoi(input.Text())
+// 	return num, err
 
 // }
