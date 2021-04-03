807d89d475285d642acf3a2b311a8b1e3ebc1c93
Evgeniy <n0fr1@mail.ru>
2021-04-03 12:30:20 +0300

    upd 4ex

--- lesson_4/horseStep/main.go
+++ lesson_4/horseStep/main.go
@@ -2,56 +2,29 @@ package main
 
 import "fmt"
 
-//AllHorsesSteps is...
-type AllHorsesSteps []HorseStep
-
-//HorseStep is...
-type HorseStep struct { //можно для уже выполенных ходов именно такую структуру и создать.
-	c1 Coordinata
-	c2 Coordinata
-	c3 Coordinata
-	c4 Coordinata
+type HorseStep struct {
+	xchoice Coordinata
+	ychoice Coordinata
+	xresult Coordinata
+	yresult Coordinata
+	index   int
 }
 
-//Coordinata is ...
 type Coordinata struct {
-	cRow map[string]int //ряд
-	cNum map[string]int //номер
-	cInd map[string]int //индекс
+	cRow int //ряд
+	cNum int //номер
+	cInd int //индекс
 }
 
-//Userchoise is ...как и чем можно заменить Userchoise?
-type Userchoise struct {
-	uRow int
-	uNum int
-	uInd int
-}
+type Field []Coordinata
 
-//Choises is ...
-type Choises []Userchoise
+type Rows []Field
 
-//VarPoint is ...
-type VarPoint struct {
-	Row     int
-	NumsRow []int
-}
+type BorderForStep []Coordinata // границы для определения хода.
 
-//VaryablesField is ...
-type VaryablesField []VarPoint
-
-//BorderForStep is...
-type BorderForStep []Coordinata //сюда закинем границы для определения хода.
-
-//TestStep is ...
-type TestStep struct { //тестируем возможность хода
-	Variants VaryablesField
-	xchoise  Userchoise
-	ychoise  Userchoise
-	result1  Coordinata
-	result2  Coordinata
-	borders  BorderForStep
-	mayMove  bool
-	testtt   int
+type Alldata struct {
+	Variants Rows          //поле
+	borders  BorderForStep //возможные границы для хода
 }
 
 func main() {
@@ -71,141 +44,178 @@ func main() {
 	// 	os.Exit(1)
 	// }
 
-	var varyables VaryablesField
-	var start1, start2 int //исходим из того, что будут заданы начальные координаты для двух из четырех клеток первого стартового хода коня.
-	//var occ []int
+	//стартовые границы поля
+	sizeField := 5
+	end := sizeField * sizeField
+	num := 0
 
-	//случай для доски 5х5. Пока указываем явно, потом переделаем.
-	varyables.addPoint(0, []int{1, 2, 3, 4, 5})
-	varyables.addPoint(1, []int{6, 7, 8, 9, 10})
-	varyables.addPoint(2, []int{11, 12, 13, 14, 15})
-	varyables.addPoint(3, []int{16, 17, 18, 19, 20})
-	varyables.addPoint(4, []int{21, 22, 23, 24, 25})
+	var fieldSlice []Coordinata
+	var getCoord Coordinata
+	rowsSlice := make([]Field, sizeField)
 
-	start1 = 13 //для теста
-	start2 = 14 //нач.координаты
+	xNum := 7 //стартовые точки отсчета
+	yNum := 8
 
-	testPosition, x, y := checkChoice(&varyables, start1, start2) //вся эта проверка - на соседство выбранных клеток. Также определяем местоположение на шахматной доске.
+	//заполняем поле 5*5
+	for row := 0; row < sizeField; row++ { //попробовать переписать через range и перекинуть в отдельные функции.
+
+		for ind := 0; ind < sizeField; ind++ {
+
+			num++
+			getCoord = fillCoordinata(row, num, ind)
+
+			fieldSlice = append(fieldSlice, getCoord) //из-за необходимости очистки, используем в данном случае append
+
+			if num%sizeField == 0 {
+				break
+			}
+		}
+
+		rowsSlice[row] = fieldSlice //более быстрый способ заполнения
+
+		fieldSlice = nil //чтобы не задублировать, очищаем слайс.
+
+		if num == end {
+			break
+		}
+	}
+
+	var gotStruct Alldata
+	gotStruct.Variants = rowsSlice
+
+	testPosition, xchoice, ychoice := gotStruct.checkChoice(xNum, yNum)
 	if !(testPosition) {
 		fmt.Println("Введены не верные начальные координаты!") //проверка на соседство клеточек не пройдена
 		return
 	}
 
-	mstep := TestStep{
-		Variants: varyables,
-		xchoise:  x,
-		ychoise:  y,
-		mayMove:  true,
-		testtt:   0,
+	hs := HorseStep{
+		xchoice: xchoice,
+		ychoice: ychoice,
+		index:   0,
 	}
 
-	var borders BorderForStep
-	makeStep(&mstep, borders)
-
-	//occ.addOcc(x.Cnum,y.Cnum) //передаем координаты выбора в занятые клетки
-	//occupiedMoves(x, y)
+	gotStruct.makeStep(&hs)
 
 }
 
-func makeStep(m *TestStep, borders BorderForStep) {
+func (a *Alldata) makeStep(h *HorseStep) {
 
-	//var mayMove bool
+	var canMove bool
 
-	if m.mayMove { //вначале определяем границы хода
-		borders = findBordersForStep(m)
-	}
+	a.findBordersForStep(h)
 
-	var lenBorders int
+	for i := 0; i < len(a.borders); i++ {
 
-	if len(borders) == 0 {
-		lenBorders = 0
-	} else {
-		lenBorders = len(borders) - 1
-	}
+		if h.xresult.cInd == 0 && h.xresult.cNum == 0 && h.xresult.cRow == 0 {
 
-	for i := 0; i <= lenBorders; i++ {
-
-		if _, ok := m.result1.cNum["Num"]; !ok { //проверяем структуру на пустоту, если пустая, то заполняем.
-			m.result1 = fillCoordinata(borders[i].cRow["Row"], borders[i].cNum["Num"], borders[i].cInd["Ind"]) //это будет первая координата.
+			h.xresult = fillCoordinata(a.borders[h.index].cRow, a.borders[h.index].cNum, a.borders[h.index].cInd)
 			continue
 		}
 
-		m.result2 = fillCoordinata(borders[i].cRow["Row"], borders[i].cNum["Num"], borders[i].cInd["Ind"]) //заполняем вторую предположительную координату хода.
+		if h.xresult.cNum == a.borders[i].cNum {
+			continue
+		}
 
-		m.mayMove = checkStep(m.result1, m.result2, m.xchoise, m.ychoise) //здесь есть возможность - проба загнать все в одну структуру
+		h.yresult = fillCoordinata(a.borders[i].cRow, a.borders[i].cNum, a.borders[i].cInd)
+		canMove = checkStep(h)
 
-		if m.mayMove { //если ход можно сделать, то прерываем.
-
-			delCoordinats(m) //Занятые по ходу позиции удаляем из поля возможных вариантов.
-
-			fmt.Println(m.mayMove, m.result1.cNum["Num"], m.result2.cNum["Num"], m.xchoise.uNum, m.ychoise.uNum)
-			fmt.Println(m.Variants)
-
-			clearResult(&m.result1)
-			clearResult(&m.result2)
-
-			m.xchoise, m.ychoise = findXY(m) //нужно также определить новые две точки x и y.
-			m.testtt = 0                     //обнуляем, ход сделан.
-			makeStep(m, borders)             //снова запуск makeStep.
-
+		if canMove { //нашли устраивающий вариант.
+			fmt.Println("step: ", h.xchoice.cNum, h.ychoice.cNum, h.xresult.cNum, h.yresult.cNum) //ход.
+			a.prepareStep(h)
 			break
 		}
 	}
 
-	m.testtt++ //стартовая точка отсчета
+	if !(canMove) {
 
-	if !m.mayMove {
+		h.index++
 
-		if m.testtt == 3 {
-			return
+		if h.index < len(a.borders) {
+			h.xresult = fillCoordinata(a.borders[h.index].cRow, a.borders[h.index].cNum, a.borders[h.index].cInd) //сдвигаем координату на один.
+			a.makeStep(h)
+		} else { //с этим координатами нет возможности хода.
+
+			if len(a.borders) != 0 {
+				h.index = 0
+				h.xresult.cInd = 0
+				h.xresult.cNum = 0
+				h.xresult.cRow = 0
+				a.prepareStep(h)
+
+			}
+
 		}
 
-		//if !mayMove && len(borders) != 0 {
-		//fmt.Println("do ", borders)
-
-		if len(borders) >= m.testtt {
-			borders = borders[m.testtt:] //каждый раз уменьшаем слайс (сначала)
-		}
-
-		clearResult(&m.result1) //очищаем мап только первой координаты
-
-		//fmt.Println("after ", borders)
-
-		makeStep(m, borders)
-		return
 	}
 
-	// if len(borders) == 0 {
-	// 	fmt.Println("нет возможности заполнить!")
-	// }
 }
 
-func findXY(m *TestStep) (Userchoise, Userchoise) {
+func (a *Alldata) prepareStep(h *HorseStep) {
+
+	a.delCoordiantsFromAllData(h) //удаляем координаты из доступных.
+	a.findNewXYChoice(h)          //определяем новые xchoice ychoice
+	a.makeStep(h)                 //снова делаем ход.
+
+}
+
+func (a *Alldata) delCoordiantsFromAllData(h *HorseStep) {
+
+	//удаляем выбранные координаты из слайса.
+	for v := 0; v < len(a.Variants); v++ {
+
+		thisRow := a.Variants[v]
+
+		for ind := 0; ind < len(thisRow); ind++ {
+
+			if thisRow[ind].cNum == h.xchoice.cNum || thisRow[ind].cNum == h.ychoice.cNum || thisRow[ind].cNum == h.xresult.cNum || thisRow[ind].cNum == h.yresult.cNum {
+				thisRow[ind] = thisRow[len(thisRow)-1]
+				thisRow = thisRow[:len(thisRow)-1]
+				a.Variants[v] = thisRow //удаляем и получаем неотсортированный слайс. Нужно ли его сортировать?
+				ind--
+			}
+
+		}
+
+		//for ind, zn := range thisRow {
+
+		//if zn.cNum == h.xchoice.cNum || zn.cNum == h.ychoice.cNum || zn.cNum == h.xresult.cNum || zn.cNum == h.yresult.cNum {
+		//	thisRow[ind] = thisRow[len(thisRow)-1]
+
+		//	thisRow = thisRow[:len(thisRow)-1]
+		//	a.Variants[v] = thisRow //удаляем и получаем неотсортированный слайс. Нужно ли его сортировать?
+
+		//}
+
+	}
+
+}
+
+func (a *Alldata) findNewXYChoice(h *HorseStep) {
 
 	var testPosition bool
-	var x, y Userchoise
+	var xchoice, ychoice Coordinata
 
-	x1 := make(map[string]int)
-	var x2 int
+	x := make(map[string]int) //чтобы удобнее было осуществлять проверку на пустоту.
+	y := make(map[string]int)
 
-	for v := 0; v <= len(m.Variants)-1; v++ { //тут надо ограничить. закинуть в отдельную функцию.
+	//удаляем выбранные координаты из слайса.
+	for v := 0; v < len(a.Variants); v++ {
 
-		thisRow := m.Variants[v].NumsRow
+		thisRow := a.Variants[v]
+		for _, zn := range thisRow {
 
-		for _, num := range thisRow {
-
-			if _, ok := x1["num"]; !ok { //проверяем структуру на пустоту, если пустая, то заполняем.
-				x1["num"] = num
+			if _, ok := x["num"]; !ok {
+				x["num"] = zn.cNum
 				continue
 			}
 
-			x2 = num
+			y["num"] = zn.cNum
 
-			testPosition, x, y = checkChoice(&m.Variants, x1["num"], x2)
+			testPosition, xchoice, ychoice = a.checkChoice(x["num"], y["num"])
 			if testPosition {
 				break
 			}
-
 		}
 
 		if testPosition {
@@ -214,151 +224,100 @@ func findXY(m *TestStep) (Userchoise, Userchoise) {
 
 	}
 
-	return x, y
+	h.xchoice = xchoice
+	h.ychoice = ychoice
+
+	//обнуляем
+	h.xresult.cInd = 0
+	h.xresult.cNum = 0
+	h.xresult.cRow = 0
+
+	h.yresult.cInd = 0
+	h.yresult.cNum = 0
+	h.yresult.cRow = 0
 }
 
-func clearResult(r *Coordinata) {
+func checkStep(h *HorseStep) bool { //переделать на более понятный вариант - всю функцию.
 
-	r.cRow = make(map[string]int) //очищаем мап.
-	r.cNum = make(map[string]int)
-	r.cInd = make(map[string]int)
-}
+	if h.xchoice.cInd == h.ychoice.cInd { //index - одинаковый
 
-//удаляем лишние.
-func delCoordinats(m *TestStep) {
-
-	var lenVariants int
-
-	if len(m.Variants) == 0 {
-		lenVariants = 0
-	} else {
-		lenVariants = len(m.Variants) - 1
-	}
-
-	for v := 0; v <= lenVariants; v++ { //тут надо ограничить. закинуть в отдельную функцию.
-
-		thisRow := m.Variants[v].NumsRow
-		if len(thisRow) == 0 {
-			continue
-		}
-
-		for ind, num := range thisRow {
-
-			if num == m.result1.cNum["Num"] || num == m.result2.cNum["Num"] || num == m.xchoise.uNum || num == m.ychoise.uNum {
-
-				thisRow[ind] = thisRow[len(thisRow)-1]                                       //заменяем на последний
-				m.Variants[v].NumsRow = m.Variants[v].NumsRow[:len(m.Variants[v].NumsRow)-1] //удаляем последний ????
-			}
-
-		}
-
-	}
-}
-
-//получаем предварительные границы. Чтобы понимать возможные границы хода.
-func findBordersForStep(m *TestStep) BorderForStep {
-	//func findBordersForStep(v *VaryablesField, x Userchoise, y Userchoise) BorderForStep {
-
-	//var result1, result2 Coordinata //2 оставшиеся координаты предполагаемого хода.
-
-	var borderSlice BorderForStep
-
-	var lenVariants int
-
-	if len(m.Variants) == 0 {
-		lenVariants = 0
-	} else {
-		lenVariants = len(m.Variants) - 1
-	}
-
-	maxRow, minRow := findMaxMinRow(m.xchoise.uRow, m.ychoise.uRow, lenVariants)       //определяем допустимые варианты по номерам
-	maxIndex, minIndex := findMaxMinIndex(m.xchoise.uInd, m.ychoise.uInd, lenVariants) //определяем допустимые варианты по колонкам
-
-	for row := minRow; row <= (maxRow); row++ {
-		thisRow := (m.Variants)[row]
-
-		for ind, num := range thisRow.NumsRow {
-
-			if num == m.xchoise.uNum || num == m.ychoise.uNum { //2 следующие координаты не должны быть равны двум выбранным (от которых отталкиваемся)
-				continue
-			}
-
-			if ind < minIndex || ind > maxIndex { //берём только близкие по индексу.
-				continue
-			}
-
-			result := fillCoordinata(row, num, ind) //заполняем координату
-			borderSlice = append(borderSlice, result)
-
-		}
-
-	}
-
-	return borderSlice
-}
-
-func fillCoordinata(row, num, ind int) Coordinata {
-
-	cRow := make(map[string]int) //создаем и заполняем мап'у.
-	cNum := make(map[string]int)
-	cInd := make(map[string]int)
-
-	cRow["Row"] = row //сюда нужно передавать обязательно ряд
-	cNum["Num"] = num
-	cInd["Ind"] = ind
-
-	cresult := Coordinata{ //Заполняем структуру
-		cRow: cRow,
-		cNum: cNum,
-		cInd: cInd,
-	}
-
-	return cresult
-}
-
-func checkStep(result1, result2 Coordinata, x, y Userchoise) bool {
-
-	if x.uInd == y.uInd { //index - одинаковый
-
-		if result1.cInd["Ind"] == result2.cInd["Ind"] {
+		if h.xresult.cInd == h.yresult.cInd {
 			return false
 		}
 
-		if (result1.cInd["Ind"]-result2.cInd["Ind"]) > 1 || (result2.cInd["Ind"]-result1.cInd["Ind"]) > 1 {
+		if (h.xresult.cInd-h.yresult.cInd) > 1 || (h.yresult.cInd-h.xresult.cInd) > 1 {
 			return false
 		}
 
-	}
-
-	if x.uInd != y.uInd { //row - одинаковый
-
-		if result1.cInd["Ind"] != result2.cInd["Ind"] {
+		if h.xresult.cRow != h.yresult.cRow {
 			return false
 		}
 
-		if (result1.cRow["Row"]-result2.cRow["Row"]) > 1 || (result2.cRow["Row"]-result1.cRow["Row"]) > 1 {
+		if (h.xresult.cInd + h.yresult.cInd + 1) < h.xchoice.cInd {
 			return false
 		}
 
-		if result1.cRow["Row"] != x.uRow && result1.cRow["Row"] != y.uRow { //расположены не на одном ряду, что и выбранные координаты.
-			if result1.cInd["Ind"] != x.uInd && result1.cInd["Ind"] != y.uInd && result2.cInd["Ind"] != x.uInd && result2.cInd["Ind"] != y.uInd { //нет пересечений по индексу.
+		if h.xresult.cInd != h.xchoice.cInd {
+
+			//flag = false
+
+			if h.yresult.cInd != h.xchoice.cInd {
 				return false
 			}
-
 		}
+
+	}
+
+	if h.xchoice.cInd != h.ychoice.cInd { //row - одинаковый
+
+		if h.xresult.cInd != h.yresult.cInd {
+			return false
+		}
+
+		if (h.xresult.cRow-h.yresult.cRow) > 1 || (h.yresult.cRow-h.xresult.cRow) > 1 {
+			return false
+		}
+
+		if (h.xchoice.cRow != h.xresult.cRow) && (h.xchoice.cRow != h.yresult.cRow) {
+
+			if (h.xresult.cInd != h.xchoice.cInd) && (h.xresult.cInd != h.ychoice.cInd) {
+				return false
+			}
+		}
+
 	}
 
 	return true
 }
 
-func testAlreadyOccupied(num int, occ []int) bool { //как сравнить? нужен ли обязательно слайс структур?
+func (a *Alldata) findBordersForStep(h *HorseStep) {
 
-	for _, i := range occ {
-		if i == num {
-			return true
+	var borderSlice BorderForStep
+
+	maxRow, minRow := findMaxMinRow(h.xchoice.cRow, h.ychoice.cRow, len(a.Variants)-1)       //определяем допустимые варианты по номерам
+	maxIndex, minIndex := findMaxMinIndex(h.xchoice.cInd, h.ychoice.cInd, len(a.Variants)-1) //определяем допустимые варианты по колонкам
+
+	for row := minRow; row <= (maxRow); row++ {
+		thisRow := (a.Variants)[row]
+
+		for _, num := range thisRow {
+
+			if num.cNum == h.xchoice.cNum || num.cNum == h.ychoice.cNum { //2 следующие координаты не должны быть равны двум выбранным (от которых отталкиваемся)
+				continue
+			}
+
+			if num.cInd < minIndex || num.cInd > maxIndex { //берём только близкие по индексу.
+				continue
+			}
+
+			result := fillCoordinata(row, num.cNum, num.cInd) //заполняем координату
+			borderSlice = append(borderSlice, result)         //можно переделать не через append
+
 		}
+
 	}
-	return false
+
+	a.borders = borderSlice
 }
 
 //ограничиваем поиск свободных клеток только в пределах близких индексов, номеров
@@ -398,9 +357,12 @@ func getBoundaryMaxMin(x, y, lenField int) (int, int) {
 	if x > y {
 		max = x
 		min = y
-	} else {
+	} else if x < y {
 		max = y
 		min = x
+	} else {
+		max = x
+		return max, min - 2
 	}
 
 	if max < lenField {
@@ -414,54 +376,50 @@ func getBoundaryMaxMin(x, y, lenField int) (int, int) {
 	return max, min
 }
 
-func (v *VaryablesField) addPoint(row int, num []int) {
+func fillCoordinata(row, num, ind int) Coordinata {
 
-	newPoint := VarPoint{
-		Row:     row,
-		NumsRow: num,
-	}
-	*v = append(*v, newPoint)
-}
-
-//добавляем к выбору: название, номер строки и номер по порядку-индекс.
-func (ch *Choises) addChoice(row int, num int, index int) {
-
-	chnew := Userchoise{
-		uRow: row,
-		uNum: num,
-		uInd: index,
+	cresult := Coordinata{ //заполняем пустую структуру
+		cRow: row,
+		cNum: num,
+		cInd: ind,
 	}
 
-	*ch = append(*ch, chnew)
+	return cresult
 }
 
-//сравниваем выбор пользователя с тем, что есть в таблице с номерами.
-func checkChoice(v *VaryablesField, x1 int, x2 int) (bool, Userchoise, Userchoise) {
+//сравниваем выбор пользователя с тем, что есть в таблице с номерами. И проверяем на соседство выбранных координат
+func (a *Alldata) checkChoice(x1 int, x2 int) (bool, Coordinata, Coordinata) {
 
-	var chSlice Choises
+	var xchoice, ychoice Coordinata
 
-	for _, thisRow := range *v {
-		for ind, num := range thisRow.NumsRow {
-			if num == x1 || num == x2 {
-				chSlice.addChoice(thisRow.Row, num, ind)
+	for _, thisRow := range a.Variants {
+
+		for _, num := range thisRow {
+
+			if num.cNum == x1 {
+				xchoice = num
+				continue
 			}
+
+			if num.cNum == x2 {
+				ychoice = num
+			}
+
 		}
 	}
 
-	x := chSlice[0] //Поскольку координаты - две. Поэтому обращаемся напрямую по индексу
-	y := chSlice[1]
-
-	if x.uRow == y.uRow { //проверка на соседство двух клеточек шахматной доски. Если клетки находятся поодаль - (А1, B10) - то это ошибка и запускать программу нет смысла.
-		if (x.uNum-y.uNum) == 1 || (y.uNum-x.uNum) == 1 {
-			return true, x, y
+	if xchoice.cRow == ychoice.cRow { //проверка на соседство двух клеточек шахматной доски. Если клетки находятся поодаль - (А1, B10) - то это ошибка и запускать программу нет смысла.
+		if (xchoice.cNum-ychoice.cNum) == 1 || (ychoice.cNum-xchoice.cNum) == 1 {
+			return true, xchoice, ychoice
 		}
 
 	} else {
-		if x.uInd == y.uInd {
-			return true, x, y
+		if xchoice.cInd == ychoice.cInd {
+			return true, xchoice, ychoice
 		}
 	}
-	return false, x, y
+
+	return false, xchoice, ychoice
 }
 
 //проверяем, что ввели именно число
